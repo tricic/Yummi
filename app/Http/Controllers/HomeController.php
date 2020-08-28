693c3f804e3510f3a63b74da22414ade2d8c5c8f
Ismar <ismar.tricic@gmail.com>
2020-08-28 23:16:35 +0200

    changes to home controller and view

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\ItemParent;
 
 class HomeController extends Controller
 {
@@ -13,7 +13,7 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+        // $this->middleware('auth');
     }
 
     /**
@@ -23,6 +23,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $itemParents = ItemParent::with('items')->get();
+
+        return view('home', ['itemParents' => $itemParents]);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+@foreach ($itemParents as $itemParent)
+    <div class="row mb-3 p-2 border">
+        <div class="col-3">
+            <img width="140" src="{{ $itemParent->image }}" alt="Pizza">
+        </div>
+        <div class="col">
+            <h3 class="mb-0">{{ $itemParent->name }}</h3>
+            <p class="mb-2">{{ $itemParent->description }}</p>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+            <div class="text-right">
+                @foreach ($itemParent->items as $item)
+                    <button class="btn btn-sm btn-primary ml-2">{{ $item->size }} <b>{{ $item->price }}</b></button>
+                @endforeach
             </div>
         </div>
     </div>
-</div>
+@endforeach
 @endsection
