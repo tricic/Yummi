93c15dfa2eb4d3c6db87d3512b9a54645bae2bfd
Ismar <ismar.tricic@gmail.com>
2020-08-30 19:11:09 +0200

    remove google fonts from <head> (fonts included via css)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,11 +21,7 @@
     {{-- app.js --}}
     <script src="{{ asset('js/app.js') }}"></script>
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
-    <!-- Styles -->
+    <!-- app.css -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
