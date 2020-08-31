36bf6a3b65d6987d06d13e2240dfdb8f61e111fb
Ismar <ismar.tricic@gmail.com>
2020-08-31 19:10:09 +0200

    add menu link to order history

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -60,6 +60,7 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a href="{{ route('order.history') }}" class="dropdown-item">Order history</a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                         onclick="event.preventDefault();
                                                         document.getElementById('logout-form').submit();">
