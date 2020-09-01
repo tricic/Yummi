476d509b999bbc1bede4ff89a00c3e5874fc531a
Ismar <ismar.tricic@gmail.com>
2020-09-01 18:39:43 +0200

    Update footer.blade.php

--- resources/views/shared/footer.blade.php
+++ resources/views/shared/footer.blade.php
@@ -1,2 +1,29 @@
-<footer>
-</footer>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h5>Links</h5>
+            <ul>
+                <li><a href="{{ route('home') }}">Home</a></li>
+                <li><a href="{{ route('menu') }}">Menu</a></li>
+            </ul>
+        </div>
+
+        <div class="col border-left border-right">
+            <h5>Contact Us</h5>
+            <div>
+                Phone: 443-123-1234
+                <br>
+                Email: <a href="#">contact@yummi.pizza</a>
+            </div>
+        </div>
+
+        <div class="col">
+            <h5>Visit Us</h5>
+            <div>
+                39280 Blanda Park
+                <br>
+                Port Bella, AK 01483
+            </div>
+        </div>
+    </div>
+</div>
